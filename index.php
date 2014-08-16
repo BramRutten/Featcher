ed83aa9d5a6a0c1037785848f5d585d5a9213f7b
BramRutten <rutten_bram@hotmail.com>
2014-08-16 14:57:48 +0200

    Begin layout + databasestructuur

---
+++ index.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Featcher - Propose features</title>
+
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+	<script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
+	<link rel="stylesheet" type="text/css" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
+
+  </head>
+  <body>
+  
+
+
+<nav class="navbar navbar-inverse" role="navigation">
+  <div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="#">Featcher</a>
+    </div>
+
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="index.php">Top 10</a></li>
+        <li><a href="latest.php">Latest</a></li>
+      </ul>
+
+     
+      <ul class="nav navbar-nav navbar-right">
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown">Log in or Sign up <span class="caret"></span></a>
+           <div class="dropdown-menu" style="padding: 15px;">
+              	<form action="[YOUR ACTION]" method="post" accept-charset="UTF-8">
+ 			  		<input id="user_username" style="margin-bottom: 15px;" type="text" name="username" size="30" placeholder="name" />
+  					<input id="user_password" style="margin-bottom: 15px;" type="password" name="password" size="30" placeholder="Password" />
+  					<input class="btn btn-primary" style="clear: left; width: 100%; height: 32px; font-size: 13px;" type="submit" name="commit" value="Log in" />
+					<li class="divider"></li>
+					<input class="btn btn-primary" style="clear: left; width: 100%; height: 32px; font-size: 13px;" type="submit" name="commit" value="Sign up" />
+					<br/>
+				</form>
+            </div>
+        </li>
+      </ul>
+
+
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
+
+
+<div class="page-header">
+  <h1>Welcome to Featcher! <small>Log in to add ideas for features you would like to see.</small></h1>
+</div>
+
+<div>
+  <h2><span class="glyphicon glyphicon-list"></span> Top 10</h2>
+</div>
+
+
+  </body>
+</html>
